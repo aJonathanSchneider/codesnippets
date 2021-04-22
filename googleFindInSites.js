7542f5621fb1ebc1b1c788b0c5ead3d811a6c60b
jonathanshnyder <contact@metaexplorer.io>
2021-04-23 00:09:52 +0200

    debugged googleFindInSites-function, docs added

--- googleFindInSites.js
+++ googleFindInSites.js
@@ -1,22 +1,27 @@
+/**
+ * this function will create a link to search a term on a number of websites with google
+ * https://www.google.com/search?q=button+site%3Amaterial-ui.com+OR+site%3Aant.design+OR+site%3Aschema.org
+ * @param {array of strings} sites the sites the search is limited to
+ * @param {string} searchTerm the term that is being searched
+ * @returns 
+ */
 function googleFindInSites(sites, searchTerm){
-	let rv="";
 	let googleBase = "https://www.google.com/search?q=";
 	let siteStart = "site%3A";
 	let siteOR = "OR";
 	let separator = "+";
-	//site%3Amaterial-ui.com+OR+site%3Aant.design+button
-	let rvArr = [googleBase+searchTerm];
+	let rvArr = [googleBase,searchTerm];
 	let sitesMapped = sites.map(site => {
 		return `${siteStart}${site}`;
 	});
 	rvArr.push(sitesMapped.reduce((prev, cur, curIdx) => {
-		if(curIdx == 0 || curIdx == sitesMapped.length - 1) return prev+cur;
+		if(curIdx == 0 ) return prev+separator+cur;
 		return prev + separator + siteOR + separator + cur;
-	},""));
+	}));
 	return rvArr.reduce((prev, cur, curIdx) => {
-		if(curIdx == 0 || curIdx == rvArr.length - 1) return prev+cur;
+		if(curIdx == 1) return prev+cur;
 		return prev + separator + cur;
-	})
+	});
 }
 
 let term = "button";
